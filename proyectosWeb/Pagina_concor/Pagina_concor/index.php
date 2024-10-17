dfc5f1ad071d2d3fb854411b325eef8bfd19ba21
Lautaro Feruglio <94877419+skinnieraxis1@users.noreply.github.com>
2024-10-17 20:50:38 -0300

    Update index.php

--- proyectosWeb/Pagina_concor/Pagina_concor/index.php
+++ proyectosWeb/Pagina_concor/Pagina_concor/index.php
@@ -1,258 +1,221 @@
 <?php
-    include("conexion.php");
-    session_start();
-    if($_SESSION["usuario"]==""){
-        header("location: http://localhost/Pagina_concor/home.php");
-    }
-    $a=0;
-    if(isset($_POST['server_activo'])){
-        $_SESSION['server_activo'] = $_POST["server_activo"];
-    }
-    $queryServer="
-    SELECT s.ID, s.nombre
-    FROM servidor as s
-    JOIN configuraciones_server as c ON s.id = c.servidor
-    JOIN miembros as m ON c.ID = m.server
-    JOIN logear as l ON m.ID = l.miembros
-    JOIN usuarios as u ON l.usuario = u.ID
-    WHERE u.ID = ".$_SESSION['usuario']."
-    ";
-    $resultServer=$link->query($queryServer);
-    if(isset($_POST['mensaje-propio'])){
-        $queryInsertMensaje = "INSERT INTO chat(dia_ingreso, canal, creador, mensaje) VALUES (curdate(), '".$_POST['canal_activo']."','".$_SESSION['usuario']."','".$_POST['mensaje-propio']."')"or
-        die("Error " . mysqli_error($link));
-        $resultInsertMensaje = $link->query($queryInsertMensaje);
-    }
-    if(isset($_POST['nombre_categoria']) and isset($_SESSION['server_activo'])){
-        $queryInsertCategoria = "INSERT INTO categoria(servidor, nombre) VALUES (".$_SESSION['server_activo'].", '".$_POST['nombre_categoria']."')"or
-        die("Error " . mysqli_error($link));
-        $resultInsertCategoria = $link->query($queryInsertCategoria);
-    }
-    if(isset($_POST['nombre_canal']) and isset($_POST['categoria_new_canal']) and isset($_POST['descripcion_canal'])){
-        $queryInsertCanal = "INSERT INTO canal(servidor, categoria, tipo_de_canal, nombre, descripcion) VALUES (".$_POST['server_activo'].", '".$_POST['categoria_new_canal']."', 'chat', '".$_POST['nombre_canal']."', '".$_POST['descripcion_canal']."')"or
-        die("Error " . mysqli_error($link));
-        echo $queryInsertServer;
-        $resultInsertCanal = $link->query($queryInsertCanal);
-    }
+  include("conexion.php");
+  session_start();
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Concord</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="../Pagina_concor/assets/css/styleInd.css">
+    <title>Tabla presentismo</title>
 </head>
 <body>
-    <div class="cuerpo-index">
-        <div class="navbar-amigos-izq"></div>
-        <div class="cuerpo">
-            <div class="navbar-servidores">
-                <nav>
-                    <div class="nav-wrapper">
-                    <ul>
-                        <li><a href="../Pagina_concor/creador_servidores.php" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a></li>
-                    </ul>
-                    <ul class="pagination">
-                        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                        <?php
-                        while($arrayServerA=mysqli_fetch_array($resultServer))
-                        {
-                            echo '<li><a href="conectar_server.php?server='.$arrayServerA["ID"].'"><img class="img_server" src="./assets/img/jordiENP.jpg" alt="">'.$arrayServerA["nombre"].'</a></li>';
-                        }
-                        ?>
-                        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-                    </ul>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="../Pagina_concor/registrarse.php">Sign In</a></li>
-                        <li><a href="../Pagina_concor/login.php">Login</a></li>
-                        <li><a href="../Pagina_concor/logout.php">Logout</a></li>
-                    </ul>
-                    </div>
-                </nav>
-            </div>
-            <div class="canal">
-                <div class="barra-canales">
-                    <div class="zona-canales">
-                            <?php
-                            if(isset($_POST['server_activo'])){
-                                $queryCategoria="
-                                SELECT ca.ID, ca.nombre
-                                FROM categoria as ca
-                                JOIN servidor as s ON ca.servidor = s.ID
-                                JOIN configuraciones_server as c ON s.ID = c.servidor
-                                JOIN miembros as m ON c.ID = m.server
-                                JOIN logear as l ON m.ID = l.miembros
-                                JOIN usuarios as u ON l.usuario = u.ID
-                                WHERE u.ID = ".$_SESSION['usuario']." and s.ID = ".$_POST['server_activo']."
-                                ";
-                                $resultCategoria=$link->query($queryCategoria);
-                                while ($arrayCategoria=mysqli_fetch_array($resultCategoria)){
-                                    echo '<div id="fab" class="fixed-action-btn">';
-                                    echo '<a class="btn-floating btn-large red">';
-                                    echo '<li class="large material-icons">'.$arrayCategoria["nombre"].'</li>';
-                                    echo '</a>';     
-                                    $queryCanal="
-                                        SELECT can.ID, can.nombre, can.tipo_de_canal
-                                        FROM canal as can
-                                        JOIN categoria as ca ON can.categoria = ca.ID
-                                        JOIN servidor as s ON ca.servidor = s.ID
-                                        JOIN configuraciones_server as c ON s.ID = c.servidor
-                                        JOIN miembros as m ON c.ID = m.server
-                                        JOIN logear as l ON m.ID = l.miembros
-                                        JOIN usuarios as u ON l.usuario = u.ID
-                                        WHERE u.ID = ".$_SESSION['usuario']." and ca.ID = ".$arrayCategoria["ID"]."
-                                        ";
-                                    $resultCanal=$link->query($queryCanal);
-                                    echo '<ul>';
-                                    while ($arrayCanal=mysqli_fetch_array($resultCanal)){   
-                                        echo '<li><a href="conectar_server.php?canal='.$arrayCanal["ID"].'&server='.$_POST["server_activo"].'" class="btn-floating red"><i class="material-icons">'.$arrayCanal["nombre"].'</i></a></li>';
-                                    } 
-                                    echo '<li><a onclick="crear_canal('.$arrayCategoria["ID"].')" class="btn-floating yellow darken-1"><i class="material-icons">add</i></a></li>';
-                                    echo '</ul>';
-                                    echo '</div>';
-                                }  
-                            }
-                            ?> 
-                        <div class="fixed-action-btn" id="a">
-                            <a id="position_conf" class="btn-floating btn-large red">
-                                <i  class="large material-icons">mode_edit</i>
-                            </a>
-                            <ul id="position_conf_ul">
-                                <li><a onclick="crear_link()" class="btn-floating red"><i class="material-icons">group_add</i></a></li>
-                                <li><a onclick="crear_categoria()" class="btn-floating green"><i class="material-icons">add_box</i></a></li>
-                            </ul>
-                        </div>  
-                    </div>               
-                </div>
-                <div class="zona-texto"> 
-                    <div class="texto">
-                        <form method="post"></form>
-                        <div class="form-chat">
-                            <div class="form-mens-chat">
-                                <div class="mens">
-                                    <input type="text" name="mensaje-propio" placeholder="Escriba su mensaje...">
-                                    <span></span>
-                                </div>
-                            </div>
-                            <input type="submit" value="">
-                        </div>
-                        <div class="zona-mensajes">
-                            <?php
-                                if(isset($_POST["canal_activo"])){
-                                    $queryMensaje="SELECT ID, mensaje FROM chat WHERE canal = ".$_POST["canal_activo"];
-                                    $resultMensaje=$link->query($queryMensaje);
-                                    while ($arrayMensaje=mysqli_fetch_array($resultMensaje)){   
-                                        echo '<div class="zona-mensaje-indv">';
-                                        echo '<div class="ej-ft-perfil"><img src="./assets/img/quagsire.png" alt=""></div>';
-                                        echo '<div class="ej-mensaje"><p>'.$arrayMensaje["mensaje"].'</p></div>';
-                                        echo '</div>';
-                                    }
-                                }
-                            ?>
-                            
-                        </div>
-                    </div>
-                    <div class="zona-escrita"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="cat" id="crear_categoria">
-        <div class="int_cat">
-            <form method="post">
-                <div class="txt_campo_login">
-                    <input type="text" name="nombre_categoria" placeholder="Nombre categoria">
-                    <span></span>
-                </div>
-                <div class="txt_campo_login">
-                    <label for="myfile">Seleccione una imagen para la categoria</label>
-                    <input type="file" id="myfile" name="Imagen_categoria">
-                </div>
-                <input type="submit" value="Crear">
-            </form>   
-            <a onclick="cerrar_categoria()" class="large material-icons" id="cerrar">clear</a>
-        </div>
-    </div>
+
+  <!-- Navbar -->
+
+  <div>
+    <ul class="nav">
+        <li class="nav-item nav-title">
+          <a class="nav-link active" aria-current="page" href="#">For one Less Abscence</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Asistencia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Personal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Horarios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Escaneo</a>
+        </li>
+
+        <!-- Cuenta -->
+
+        <li class="nav-item nav-profile">
+          <a class="nav-link" href="./register.php">Cerrar Sesión</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+          </svg>
+        </li>
+    </ul>
+  </div>
     
-    <div class="cat" id="crear_link">
-        <div class="int_cat">
-            <?php
-            echo '<h3>enviale esto a tu amigo: localhost/Pagina_concor/ingresar_server.php?server='.$_POST["server_activo"].'</h3>';
-            ?>
-            <a onclick="cerrar_link()" class="large material-icons" id="cerrar">clear</a>
-        </div>
-    </div>
 
-    <div class="cat" id="crear_canal">
-        <div class="int_cat">
-            <form method="post">
+  <div class="bodyTable">
+    <form action="" method="post" class="form-date">
+      <label for="date" class="form-date-label">Elige la fecha deseada:</label>
 
-                <div class="txt_campo_login">
-                    <input type="text" name="nombre_canal" placeholder="Nombre Canal">
-                    <span></span>
-                </div>
+      <input
+        class="form-date-input"
+        type="date"
+        id="date"
+        name="date"
+        min="2000-01-01"
+        max="2030-12-31" 
+      />
+      <input type="submit" class="form-date-submit">
+    </form>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Apellido</th>
+        <th scope="col">Cargo</th>
+        <th scope="col">Área</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Horario de entrada</th>
+        <th scope="col">Horario de salida</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+if (isset($_POST['date'])) {
+    $date = $_POST["date"];
+    
+    // Validar el formato de la fecha (ej. YYYY-MM-DD)
+    if (!DateTime::createFromFormat('Y-m-d', $date)) {
+        echo "Fecha no válida.";
+        exit;
+    }
+    
+    $queryEmpleados = "
+    SELECT e.nombre, e.apellido, e.rol, i.hora, h.horario_ingreso, e.area
+    FROM empleados AS e
+    JOIN horario AS h ON h.id_empleados = e.id_empleados
+    JOIN ingresar AS i ON i.id_empleados = e.id_empleados
+    WHERE i.fecha = ? and h.dia_dl_semana = 'Jueves'
+    ";
+    
+    $stmt = $link->prepare($queryEmpleados);
+    $stmt->bind_param("s", $date);
+} else {
+    $queryEmpleados = "
+    SELECT e.nombre, e.apellido, e.rol, i.hora, h.horario_ingreso, e.area
+    FROM empleados AS e
+    JOIN horario AS h ON h.id_empleados = e.id_empleados
+    JOIN ingresar AS i ON i.id_empleados = e.id_empleados
+    WHERE i.fecha = CURDATE() AND h.dia_dl_semana = 'Jueves'
+    ";
+    
+    $stmt = $link->prepare($queryEmpleados);
+}
 
-                <div class="txt_campo_login">
-                    <input type="text" name="descripcion_canal" placeholder="Descripcion">
-                    <span></span>
-                </div>
+$stmt->execute();
+$resultEmpleados = $stmt->get_result();
 
-                <div class="txt_campo_login">
-                    <label for="Imagen_canal">Seleccione una imagen para el canal</label>
-                    <input type="file" id="myfile" name="Imagen_canal">
-                </div>
+if ($resultEmpleados) {
+    // Verifica si hay resultados
+    if ($resultEmpleados->num_rows > 0) {
+        while ($arrayEmpleados = $resultEmpleados->fetch_assoc()) {
+            // Determinar el estado de asistencia
+            if ($arrayEmpleados["hora"] == "00:00:00") {
+                $estado = "Ausente";
+            } elseif ($arrayEmpleados["hora"] > $arrayEmpleados["horario_ingreso"]) {
+                $estado = "Tarde";
+            } else {
+                $estado = "Presente";
+            }
+            
+            // Mostrar la fila
+            echo "<tr>";
+            echo "<td class='barraTr ".$estado."'></td>";
+            echo "<td>" . htmlspecialchars($arrayEmpleados['nombre']) . "</td>";
+            echo "<td>" . htmlspecialchars($arrayEmpleados['apellido']) . "</td>";
+            echo "<td>" . htmlspecialchars($arrayEmpleados['rol']) . "</td>";
+            echo "<td>" . htmlspecialchars($arrayEmpleados['area']) . "</td>";
+            echo "<td>$estado</td>";
+            echo "<td>" . htmlspecialchars($arrayEmpleados['hora']) . "</td>";
+            echo "<td> - </td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "<tr><td colspan='5'>No se encontraron empleados para la fecha especificada.</td></tr>";
+    }
+} else {
+    echo "Error en la consulta: " . htmlspecialchars($link->error);
+}
 
-                <div class="txt_campo_login">
-                    <input type="hidden" id="categoria_hidden" name="descripcion_canal" value="" placeholder="Descripcion">
-                </div>
-                <input type="submit" value="Crear">
-            </form>
-            <a onclick="cerrar_canal()" class="large material-icons" id="cerrar">clear</a>
-        </div>
-    </div>
+$stmt->close();
+?>
+
+    </tbody>
+  </table>
+</div>
+
 </body>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.fixed-action-btn');
-        var instances = M.FloatingActionButton.init(elems, {
-            direction: 'left',
-            hoverEnabled: false
-        });
-    });
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('#a');
-        var instances = M.FloatingActionButton.init(elems, {
-            direction: 'top',
-            hoverEnabled: false
-        });
-    });
 
-    function crear_categoria(){
-        document.getElementById("crear_categoria").style.display="block";
-    }
-    function cerrar_categoria(){
-        document.getElementById("crear_categoria").style.display="none";
-    }
-    function crear_canal(num){
-        document.getElementById("crear_canal").style.display="block";
-        document.getElementById("categoria_hidden").value=num;
-    }
-    function cerrar_canal(){
-        document.getElementById("crear_canal").style.display="none";
-    }
-    function crear_link(){
-        document.getElementById("crear_link").style.display="block";
-    }
-    function cerrar_link(){
-        document.getElementById("crear_link").style.display="none";
-    }
+<!-- Style de prueba -->
 
-</script>
+<style>
+
+  body{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .bodyTable{
+    padding: 50px !important;
+  }
+
+  .barraTr{
+    height: 100%;
+    width: 10px;
+    background-color: #ECF238;
+  }
+
+  .nav{
+    border-bottom: 1px black solid;
+    height: 8svh;
+    align-items: center !important;
+    color: white; 
+    --bs-nav-link-color: white !important;
+    --bs-nav-link-hover-color: #E688F2 !important;
+    background-color: black;
+  }
+
+  .nav-profile{
+    align-items: center !important;
+    display: flex;
+    flex-direction: right;
+  }
+
+  .nav-item:hover{
+    transform: scale(1.1);
+  }
+
+  .nav-title{
+    font-size: 25px;
+  }
+
+  .Tarde{
+    background-color: #ECF238 !important;
+  }
+
+  .Ausente{
+    background-color: red !important;
+  }
+
+  .Presente{
+    background-color: #55D983 !important;
+  }
+
+  .form-date{
+    border-bottom: 1px black solid;
+    margin: 2svh;
+    padding: 2svh 0;
+  }
+
+</style>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- <link rel="stylesheet" href="./assets/css/style.css"> -->
 </html>
